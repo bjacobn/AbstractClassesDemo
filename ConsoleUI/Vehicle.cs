67d41dd484aa6a29447a6086f1c04d22668d3d2f
Jacob <bjacobcode@gmail.com>
2021-12-01 09:20:48 -0800

    Created Vehicle Abstract Class

---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public abstract class Vehicle
+    {
+
+        public string Year { get; set; } = "Default Year";
+        public string Make { get; set; } = "Default Make";
+        public string Model { get; set; } = "Default Model";
+
+
+        //No Implementation
+        public abstract void DriveAbstract();
+
+
+        //Base Implementation
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine($"Vehicle is in drive");
+        }
+
+        
+    }
+
+}
